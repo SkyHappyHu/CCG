4d5cdc69bce2713243d79b95eb69b8e37fca8a1d
SkyHappyHu <hutianlegdut@163.com>
2025-03-03 17:01:20 +0800

    Delete util/experiment_data directory

--- util/experiment_data/normcol.m
+++
@@ -1,4 +0,0 @@
-function [D] = normcol(D)
-% normalize column to 1
-
-D = D./repmat(sqrt(sum(D.^2,1)), size(D,1), 1);
--- util/experiment_data/updateR.m
+++
@@ -1,5 +0,0 @@
-function R = updateR(B, Y, G)
-singular = B' * Y * G;
-[UB, ~, UA] = svd(singular);    
-R = UA * UB';
-end
